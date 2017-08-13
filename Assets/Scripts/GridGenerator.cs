6f005eb204a25a85fff4df7a17be8cc7927cb440
dnipromv <dnipromv@gmail.com>
2017-08-13 23:25:50 +0200

    initial commit

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,109 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class GridGenerator : MonoBehaviour {
+
+    public TextAsset dataFile;
+    public Vector2 tileSize = new Vector2(1, 1);
+
+    private GridModel _gridData;
+    private Vector2 _gridSize;
+    private int _tilesetTileResolution = 256;
+
+	private void Start () {
+        Invalidate();
+    }
+
+    private void OnValidate()
+    {
+        Invalidate();
+    }
+
+    private void Invalidate()
+    {
+        if (dataFile)
+        {
+            _gridData = new GridModel(dataFile.text);
+            _gridSize = new Vector2(_gridData.Width * tileSize.x, _gridData.Height * tileSize.y);
+            BuildMesh();
+        }
+        else
+        {
+            Debug.LogError("Grid data file is not assigned!");
+        }
+    }
+
+    private void BuildMesh()
+    {
+        int vertNum = _gridData.Width * _gridData.Height * 4;
+        int triangleNum = Mathf.RoundToInt(vertNum * 0.5f);
+
+        Material groundMat = (Material)AssetDatabase.LoadAssetAtPath("Assets/Materials/ground.mat", typeof(Material));
+        float uvTileSizeX = 1 / ((float)groundMat.mainTexture.width / _tilesetTileResolution);
+        float uvTileSizeY = 1 / ((float)groundMat.mainTexture.height / _tilesetTileResolution);
+        int uvRowNum = (int)(1 / uvTileSizeX);
+
+        Vector3[] vertices = new Vector3[vertNum];
+        int[] triangles = new int[triangleNum * 3];
+        Vector3[] normals = new Vector3[vertNum];
+        Vector2[] uv = new Vector2[vertNum];
+
+        for(int r = 0; r < _gridData.Height; r++)
+        {
+            for(int c = 0; c < _gridData.Width; c++)
+            {
+                int squareIndex = r * _gridData.Width + c;
+                int vertIndex = squareIndex * 4;
+                int triangleIndex = squareIndex * 6;
+
+                // Set up vertexes & normals
+                vertices[vertIndex] = new Vector3(c * tileSize.x, 0, (_gridData.Height - r - 1) * tileSize.y);
+                vertices[vertIndex + 1] = new Vector3((c + 1) * tileSize.x, 0, (_gridData.Height - r - 1) * tileSize.y);
+                vertices[vertIndex + 2] = new Vector3(c * tileSize.x, 0, (_gridData.Height - r) * tileSize.y);
+                vertices[vertIndex + 3] = new Vector3((c + 1) * tileSize.x, 0, (_gridData.Height - r) * tileSize.y);
+
+                for (int i = 0; i < 4; i++)
+                    normals[vertIndex + i] = Vector3.up;
+
+                // Set up UV coordinates for vertexes 
+                int ti = _gridData.Layers["ground"].Tiles[r, c];
+                int tiX = ti % uvRowNum;
+                int tiY = uvRowNum - Mathf.FloorToInt((float)ti / uvRowNum) - 1;
+
+                uv[vertIndex] = new Vector2(tiX * uvTileSizeX, tiY * uvTileSizeY);
+                uv[vertIndex + 1] = new Vector2((tiX + 1) * uvTileSizeX, tiY * uvTileSizeY);
+                uv[vertIndex + 2] = new Vector2(tiX * uvTileSizeX, (tiY + 1) * uvTileSizeY);
+                uv[vertIndex + 3] = new Vector2((tiX + 1) * uvTileSizeX, (tiY + 1) * uvTileSizeY);
+
+                // Set up triangles
+                triangles[triangleIndex] = vertIndex;
+                triangles[triangleIndex + 1] = vertIndex + 3;
+                triangles[triangleIndex + 2] = vertIndex + 1;
+
+                triangles[triangleIndex + 3] = vertIndex;
+                triangles[triangleIndex + 4] = vertIndex + 2;
+                triangles[triangleIndex + 5] = vertIndex + 3;
+            }
+        }
+
+        Mesh mesh = new Mesh()
+        {
+            vertices = vertices,
+            triangles = triangles,
+            normals = normals,
+            uv = uv
+        };
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+
+        meshFilter.mesh = mesh;
+        meshCollider.sharedMesh = mesh;
+        meshRenderer.sharedMaterial = groundMat;
+    }
+}
---
+++ Assets/Scripts/GridModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using UnityEngine;
+public class GridModel
+{
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+    public Dictionary<string, LayerModel> Layers { get; private set; }
+
+    public GridModel(string jsonData)
+    {
+        var jObject = JSON.Parse(jsonData);
+        Width = jObject["width"].AsInt;
+        Height = jObject["height"].AsInt;
+
+        var layersArray = jObject["layers"].AsArray;
+        Layers = new Dictionary<string, LayerModel>(layersArray.Count);
+        foreach(JSONNode node in layersArray) Layers[node["name"]] = new LayerModel(node);
+
+
+        //Debug.Log(jObject.ToString());
+    }
+
+    public class LayerModel
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int[,] Tiles { get; private set; }
+
+        public LayerModel(JSONNode layerData)
+        {
+            Width = layerData["width"].AsInt;
+            Height = layerData["height"].AsInt;
+
+            Tiles = new int[Height, Width];
+            var tilesArray = layerData["data"].AsArray;
+            for (int i = 0; i < tilesArray.Count; i++)
+            {
+                int row = Mathf.FloorToInt(i / Width);
+                int col = i % Width;
+                Tiles[row, col] = tilesArray[i].AsInt - 1;
+            }
+        }
+    }
+}
